317c116578ce5d36c35d7c09ac6a75b63886e26e
Shanthi Collooru <shanthi_collooru@yahoo.com>
2019-10-30 16:48:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+class _Node {
+  // _ indicated private class, should only be accessible by LinkedList
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null; // indicated head, start at empty list (null)
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  find(item) {
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list 
+           and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      } else {
+        // Otherwise, keep looking
+        currNode = currNode.next;
+      }
+    }
+    // Found it
+    return currNode;
+  }
+
+  remove(item) {
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // If the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    // Start at the head
+    let currNode = this.head;
+    // Keep track of previous
+    let previousNode = this.head;
+
+    while (currNode !== null && currNode.value !== item) {
+      // Save the previous node
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log("Item not found");
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
+}
+
+function main() {
+  let linkedList = new LinkedList();
+
+  linkedList.insertFirst("Apollo");
+  linkedList.insertLast("Boomer");
+  linkedList.insertLast("Helo");
+  linkedList.insertLast("Husker");
+  linkedList.insertLast("Starbuck");
+
+  linkedList.insertLast("Tauhida");
+  linkedList.remove("Husker");
+
+  console.log(linkedList);
+}
